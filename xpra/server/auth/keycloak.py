893f7edae11cfa30aa71958dcc51323e99a37ad4
Nathalie Casati <nathalie.casati@chuv.ch>
2022-12-13 15:19:23 +0000

    auth groups

--- xpra/server/auth/keycloak.py
+++ xpra/server/auth/keycloak.py
@@ -182,7 +182,7 @@ class Authenticator(SysAuthenticator):
                 groups_claim = get(user_info, group_claims.split('.'))
                 log("claims: %r", groups_claim)
                 if self.auth_condition == "or":
-                  if len({self.auth_groups}.intersection(set(groups_claim)) == 0:
+                  if len({self.auth_groups}.intersection(set(groups_claim))) == 0:
                     log.error("Error: keycloak authentication failed as groups claim is not satisfied")
                     return False
                 elif self.auth_condition == "and": 

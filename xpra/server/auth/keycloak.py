5c63407c8acfde87c4957db4991e36e42a3f4ee3
Nathalie Casati <nathalie.casati@chuv.ch>
2022-12-13 13:36:16 +0000

    auth groups

--- xpra/server/auth/keycloak.py
+++ xpra/server/auth/keycloak.py
@@ -170,8 +170,8 @@ class Authenticator(SysAuthenticator):
             log("user_info: %r", user_info)
 
             log("groups_claim: %r", self.groups_claim)
-            log("auth_groups: %r", self.auth_groups)
-            log("groups_claim: %r", user_info['roles']['group'])
+            log("auth_groups: %r", set(self.auth_groups))
+            log("groups_claim: %r", set(user_info['roles']['group']))
             if self.groups_claim is not None and self.groups_claim:
               if not self.auth_groups or self.auth_groups is None:
                 log.error("Error: keycloak authentication failed as auth_groups is invalid")
